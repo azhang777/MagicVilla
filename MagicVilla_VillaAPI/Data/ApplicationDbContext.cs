5042653d274967c7d6858fd5b9605d71723c4041
Andy <andyzhanggm@gmail.com>
2023-12-14 03:08:43 -0500

    trying to connect to database, but it wont work!

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+        //this generates a table called Villas
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
